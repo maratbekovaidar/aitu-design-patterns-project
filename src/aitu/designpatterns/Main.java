917e67db5b4da58010558ce49f8a496ca96f882a
LargaEspera <74295561+LargaEspera@users.noreply.github.com>
2021-11-26 11:53:50 +0600

    Update Main.java

--- src/aitu/designpatterns/Main.java
+++ src/aitu/designpatterns/Main.java
@@ -2,6 +2,20 @@ package aitu.designpatterns;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        UserFactory userFactory = new UserFactory();
+        User user1 = userFactory.createUser("Aidar1", "Maratbekov", "user");
+        User user2 = userFactory.createUser("Aidar2", "Maratbekov", "user");
+        Admin admin = (Admin) userFactory.createUser("Aidar", "Maratbekov", "admin");
+
+//        admin.update(userFactory.defaults);
+        
+        OrderFacade order = new OrderFacade();
+
+        IDoner stardardDonerWithCheeseAndWithJalapeno = new CheeseDecorator(new JalapenoDecorator(new StandardDoner()));
+
+        order.executeOrder(stardardDonerWithCheeseAndWithJalapeno);
+
+        CoffeeOrder coffeeOrder = new CoffeeOrder();
+        coffeeOrder.make();
     }
 }
